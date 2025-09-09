95fbc245355a9b3d1dc8af26c0a0f8bdeea47206
BHUVANESHWAR <BHUVANESHWAR@DESKTOP-9L1LNJV>
2025-09-09 16:41:19 +0530

    Chapter V Patterns I Completed

---
+++ DSA_With_JAVA/Chapter_V_PatternsI/CharacterPattern.java
@@ -0,0 +1,17 @@
+package DSA_With_JAVA.Chapter_V_PatternsI;
+
+public class CharacterPattern {
+    public static void main(String[] args) {
+        int n = 4;
+        char ch = 'A';
+
+        for(int line=1;line<=n;line++){
+            for(int chars=1;chars<=line;chars++){
+                System.out.print(ch+" ");
+                ch++;
+            }
+            System.out.println();
+        }
+
+    }
+}
---
+++ DSA_With_JAVA/Chapter_V_PatternsI/RightAngleTrianglePattern.java
@@ -0,0 +1,18 @@
+package DSA_With_JAVA.Chapter_V_PatternsI;
+
+public class RightAngleTrianglePattern {
+    public static void main(String[] args) {
+        for(int i=1;i<=5;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print(" * ");
+            }
+            System.out.println();
+        }
+    }
+}
+// outer loop indicated kitne lines hai 
+// in lop ek line main kitne hai 
+
+// for(int line=1;line<=4;line++){
+//  for(int star=1;star<=line;star++){sout("*")
+//}}
